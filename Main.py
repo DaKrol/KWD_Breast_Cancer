b96dc754a7dd32eac7a5253d9c963d2a4d313927
DKrol <37586342+DaKrol@users.noreply.github.com>
2019-02-06 19:53:17 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -9,6 +9,9 @@ from sklearn.multiclass import OneVsRestClassifier
 from sklearn.metrics import confusion_matrix
 from sklearn.datasets import load_breast_cancer
 
+from sklearn.metrics import accuracy_score
+import seaborn as sns;
+
 cancer_data = load_breast_cancer()
 
 print(cancer_data.DESCR)
@@ -16,13 +19,10 @@ print(cancer_data.DESCR)
 print(cancer_data.data.shape)
 print(cancer_data.target.shape)
 
-print("First patient in database")
-print(cancer_data['data'][0,:])
-
 
 train_data, test_data, \
 train_target, test_target = \
-train_test_split(cancer_data.data, cancer_data.target, test_size=0.1)
+train_test_split(cancer_data.data, cancer_data.target, test_size=0.2)
 
 logistic_regression = LogisticRegression()
 logistic_regression.fit(train_data, train_target)
@@ -31,9 +31,18 @@ conf_matrix = confusion_matrix(test_target, logistic_regression.predict(test_dat
 print("Confusion_matrix:")
 print(conf_matrix)
 
-from sklearn.metrics import accuracy_score
 acc = accuracy_score(test_target, logistic_regression.predict(test_data))
 print("Model accuracy is {0:0.2f}".format(acc))
 
-import seaborn as sns;
+
 sns.heatmap(conf_matrix)
+
+print("Using classifier")
+id=8
+pred = logistic_regression.predict(test_data[id,:].reshape(1,-1))
+print("Model predicted for patient {0} value {1}".format(id, pred))
+
+print("Real value for patient \"{0}\" is {1}".format(id, test_target[id]))
+
+prediction_probability = logistic_regression.predict_proba(test_data[id,:].reshape(1,-1))
+print(prediction_probability)
